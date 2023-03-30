f6e7d76aa5175c2b6ca40439565fce882afa3156
typsdkvmj <dasha.all555@gmail.com>
2023-03-30 17:51:49 +0300

    тестируем передачи объектной переменной в функции

--- LessonOOPExampleProject/main.cpp
+++ LessonOOPExampleProject/main.cpp
@@ -7,6 +7,14 @@ public:
 	int age;
 	int avg_mark;
 };
+
+void clear(Student st) {
+	st.name = "no name";
+	st.surname = "no surname";
+	st.age = 0;
+	st.avg_mark = 0;
+}
+
 int main() {
 	Student st1;
 
@@ -15,33 +23,23 @@ int main() {
 	st1.age = 14;
 	st1.avg_mark = 10;
 
-	Student st2 = st1;
 
 	cout << "before: " << endl;
 
-		cout << st1.name << endl;
-		cout << st1.surname << endl;
-		cout << st1.age << endl;
-		cout << st1.avg_mark << endl;
+	cout << st1.name << endl;
+	cout << st1.surname << endl;
+	cout << st1.age << endl;
+	cout << st1.avg_mark << endl;
 
-		cout << st2.name << endl;
-		cout << st2.surname << endl;
-		cout << st2.age << endl;
-		cout << st2.avg_mark << endl;
 
-		st1.avg_mark = 7;
+	clear(st1);
 
-		cout << "\nafter: " << endl;
+	cout << "\nafter: " << endl;
 
-		cout << st1.name << endl;
-		cout << st1.surname << endl;
-		cout << st1.age << endl;
-		cout << st1.avg_mark << endl;
-
-		cout << st2.name << endl;
-		cout << st2.surname << endl;
-		cout << st2.age << endl;
-		cout << st2.avg_mark << endl;
+	cout << st1.name << endl;
+	cout << st1.surname << endl;
+	cout << st1.age << endl;
+	cout << st1.avg_mark << endl;
 
 	return 0;
 }
