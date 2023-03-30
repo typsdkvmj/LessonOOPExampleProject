63d44bdfdf85e226f4aafb0443273aa95af1d5af
typsdkvmj <dasha.all555@gmail.com>
2023-03-30 17:38:04 +0300

    протестировали операцию присваивания для переменных объектного типа

--- LessonOOPExampleProject/main.cpp
+++ LessonOOPExampleProject/main.cpp
@@ -8,12 +8,40 @@ public:
 	int avg_mark;
 };
 int main() {
-	Student st;
+	Student st1;
 
-		cout << st.name << endl;
-		cout << st.surname << endl;
-		cout << st.age << endl;
-		cout << st.avg_mark << endl;
+	st1.name = "ivan";
+	st1.surname = "ivanov";
+	st1.age = 14;
+	st1.avg_mark = 10;
+
+	Student st2 = st1;
+
+	cout << "before: " << endl;
+
+		cout << st1.name << endl;
+		cout << st1.surname << endl;
+		cout << st1.age << endl;
+		cout << st1.avg_mark << endl;
+
+		cout << st2.name << endl;
+		cout << st2.surname << endl;
+		cout << st2.age << endl;
+		cout << st2.avg_mark << endl;
+
+		st1.avg_mark = 7;
+
+		cout << "\nafter: " << endl;
+
+		cout << st1.name << endl;
+		cout << st1.surname << endl;
+		cout << st1.age << endl;
+		cout << st1.avg_mark << endl;
+
+		cout << st2.name << endl;
+		cout << st2.surname << endl;
+		cout << st2.age << endl;
+		cout << st2.avg_mark << endl;
 
 	return 0;
 }
