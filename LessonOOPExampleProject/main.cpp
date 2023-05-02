1e843c2f4d449632cf2208d7cd3d4f7fe467623a
typsdkvmj <dasha.all555@gmail.com>
2023-05-02 18:47:04 +0300

    реализвали класс менеджер и добавили описание

--- LessonOOPExampleProject/main.cpp
+++ LessonOOPExampleProject/main.cpp
@@ -1,12 +1,30 @@
-#include "student.h"
+#include "manager.h"
 
 
 int main() {
+
+		Student st1("Ivan", "Ivanov", 14, 7.6);
+	Student st2("Alex", "Invisible", 15, 10);
+	Student st3("Peter", "Pen", 12, 4);
+	Student st4("Harry", "Potter", 14, 9.6);
+	Student st5("Alice", "Inder", 13, 8);
 	
+	Group group;
+	group.add(st1);
+	group.add(st2);
+	group.add(st3);
+	group.add(st4);
+	group.add(st5);
 
+	Manager manager;
 
+	cout << "Group average age: " << manager.calcStudentsAvgAge(group) << endl;
+	cout << "Group average mark: " << manager.calcStudentsAvgMark(group) << endl;
+	Student s = manager.findBestStudent(group);
+	cout << "Best group student: " << s.convert() << endl;
 
-
+	s = manager.findWorstStudent(group);
+	cout << "Worst group student: " << s.convert() << endl;
 
 	return 0;
 }
--- LessonOOPExampleProject/manager.cpp
+++ LessonOOPExampleProject/manager.cpp
@@ -13,3 +13,34 @@ float Manager::calcStudentsAvgAge(Group group) {
 
 	return avg / group.getSize();
 }
+
+	float Manager::calcStudentsAvgMark(Group group) {
+		float avg = 0;
+
+		for (int i = 0; i < group.getSize(); i++)
+		{
+			Student st = group.getStudent(i);
+			avg += st.getAvgMark();
+		}
+
+		return avg / group.getSize();
+	}
+	Student Manager::findBestStudent(Group group) {
+		Student st = group.getStudent(0);
+
+		for (int i = 1; i < group.getSize(); i++) {
+			if (st.getAvgMark() < group.getStudent(i).getAvgMark())
+				st = group.getStudent(i);
+		}
+		return st;
+	}
+	Student Manager::findWorstStudent(Group group) {
+		Student st = group.getStudent(0);
+
+		for (int i = 1; i < group.getSize(); i++) {
+			if (st.getAvgMark() > group.getStudent(i).getAvgMark())
+				st = group.getStudent(i);
+		}
+		return st;
+
+}
